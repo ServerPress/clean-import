bc78bd3f80c1152f3dbb9a36e315388b3014b1d8
Gregg Franklin <greggfranklin@gmail.com>
2016-01-20 20:37:52 -0800

    Removed blanket rename on mu-plugins folder and set to specific files for WPEngine

--- inc/wpengine.php
+++ inc/wpengine.php
@@ -9,12 +9,26 @@
  */
  
  	// Collect needed configuration info
- 	$mu_plugins = $ds_runtime->preferences->sites->{$siteName}->sitePath . '/wp-content/mu-plugins';
+ 	$mu_path = $ds_runtime->preferences->sites->{$siteName}->sitePath . '/wp-content/mu-plugins';
+ 	$force_strong_passwords_folder = $mu_path . '/force-strong-passwords';
+ 	$force_strong_passwords_script = $mu_path . '/slt-force-strong-passwords.php';
+ 	$wpengine_common = $mu_path . '/wpengine-common';
+ 	$mu_plugin_script = $mu_path . '/mu-plugin.php';
+ 	$stop_long_comments_script = $mu_path . '/stop-long-comments.php';
 
-	//Saves config
-	$clean_config_file->save( $wpconfig );
 
-  	// See if mu-plugins folder exists, if does rename
-  	if ( is_dir( $mu_plugins ) ) {
-		rename( $mu_plugins, $mu_plugins . '-sav-' . time() ); 
-  	}
+    //Rename cache, security files and folders
+    $files_to_rename = array(
+    	$advanced_cache,
+			$force_strong_passwords_folder,
+			$force_strong_passwords_script,
+			$wpengine_common,
+			$mu_plugin_script,
+			$stop_long_comments_script
+    	);
+            
+        foreach	( $files_to_rename as $file_to_rename ) {
+			if ( file_exists( $file_to_rename ) ) {
+				rename( $file_to_rename, $file_to_rename . '-sav-' . time() );  
+			}
+        } 
