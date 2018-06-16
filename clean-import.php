f598340f90528ae7539141e36df5b4becd12c098
Dave Jesch <dave@davejesch.com>
2018-06-16 12:59:34 -0700

    add install message #24

--- clean-import.php
+++ clean-import.php
@@ -5,8 +5,32 @@ Plugin URI: http://serverpress.com/plugins/clean-import
 Description: Resets .htacces, clears cache, disables plugins and resets WordPress environment for local hosting with DesktopServer.
 Author: ServerPress Team
 Version: 1.1.2
+Text Domin: clean-import
 Author URI: http://serverpress.com/
 Text Domain: clean-import
 */
 
-// intentionally blank. DesktopServer will load the prepend.php file automatically.
+if ( FALSE === stripos( __DIR__, 'ds-plugins' ) ) {
+	// detect if not in the ds-plugins folder
+	if ( is_admin() )
+		add_action( 'admin_notices', array( $this, 'clean_import_install_message' ) );
+	return;		// do not initialize the rest of the plugin
+}
+
+/**
+ * Display admin notification to install plugin in correct directory
+ */
+function clean_import_install_message()
+{
+	if ( 'Darwin' === PHP_OS )
+		$correct_dir = '/Applications/XAMPP/ds-plugins/';		// mac directory
+	else
+		$correct_dir = 'C:\\xampplite\\ds-plugins\\';			// Windows directory
+
+	echo '<div class="notice notice-error">',
+		'<p>',
+		sprintf( __('<b>Notice:</b> The Clean Import plugin needs to be installed in Desktop Server\'s ds-plugins directory.<br/>Please install in %1$sclean-import', 'clean-import' ),
+			$correct_dir),
+		'</p>',
+		'</div>';
+}
