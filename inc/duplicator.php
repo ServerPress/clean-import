dd8804d4d986048a9e22bb3e1b84ef2e839913af
Dave Jesch <dave@davejesch.com>
2021-06-07 11:11:38 -0700

    check for dup-installer/ or ds-dup-installer/ directories; rework scanning for database.sql #41

--- inc/duplicator.php
+++ inc/duplicator.php
@@ -5,7 +5,7 @@ class DS_Clean_Import_Duplicator extends DS_Clean_Import_Base
 	public function pre_import_process( $info )
 	{
 DS_Clean_Import::debug(__METHOD__.'():' . var_export($info, TRUE));
-		$workdir = $info[2];
+		$workdir = $info[2];			// get the temp directory archive was exported to
 
 		$files = NULL;
 
@@ -34,63 +34,72 @@ DS_Clean_Import::debug(__METHOD__.'(): renamed ' . $workdir . $file);
 				}
 			}
 
+			// set up $dup_installer to contain proper directory name
+			$dup_installer = $workdir . 'dup-installer';
+			if ( ! is_dir( $dup_installer ) )
+				$dup_installer = $workdir . 'ds-dup-installer';
+
 			// scan for dup-installer/original_files_* directory
-DS_Clean_Import::debug(__METHOD__.'():' . __LINE__ . ' searching for "' . $workdir . 'dup-installer" directory');
-			if ( is_dir( $workdir . 'dup-installer' ) ) {
-				$scandir = $workdir . 'dup-installer' . DIRECTORY_SEPARATOR . 'original_files_*';
+DS_Clean_Import::debug(__METHOD__.'():' . __LINE__ . ' searching for "' . $dup_installer . '" directory');
+			if ( is_dir( $dup_installer ) ) {
+				$scandir = $dup_installer . DIRECTORY_SEPARATOR . 'original_files_*';
 				$files = scandir( $scandir );
 DS_Clean_Import::debug(__METHOD__.'():' . __LINE__ . ' searching for ' . $scandir . ' finds: ' . var_export($files, TRUE));
-				foreach ( $files as $file ) {
-					if ( is_dir( $file ) ) {
-						$found = FALSE;
-						// if it's a directory, look for specific files within it
-						$source = $file . DIRECTORY_SEPARATOR . 'source_site_wpconfig';
+				if ( FALSE !== $files ) {
+					foreach ( $files as $file ) {
+						if ( is_dir( $file ) ) {
+							$found = FALSE;
+							// if it's a directory, look for specific files within it
+							$source = $dup_installer . DIRECTORY_SEPARATOR . $file . DIRECTORY_SEPARATOR . 'source_site_wpconfig';
 DS_Clean_Import::debug(__METHOD__.'():' . __LINE__ . ' checking "' . $source . '"');
-						if ( file_exists( $source ) ) {
-							rename( $source, $workdir . 'wp-config.php' );
-							$found = TRUE;
+							if ( file_exists( $source ) ) {
+								rename( $source, $workdir . 'wp-config.php' );
+								$found = TRUE;
 DS_Clean_Import::debug(__METHOD__.'():' . __LINE__ . ' renamed "' . $source . '" to "' . $workdir . 'wp-config.php"');
-						}
+							}
 
-						$source = $file . DIRECTORY_SEPARATOR . 'source_site_userini';
+							$source = $dup_installer . DIRECTORY_SEPARATOR . $file . DIRECTORY_SEPARATOR . 'source_site_userini';
 DS_Clean_Import::debug(__METHOD__.'():' . __LINE__ . ' checking "' . $source . '"');
-						if ( file_exists( $source ) ) {
-							rename( $source, $workdir . 'user.ini' );
-							$found = TRUE;
+							if ( file_exists( $source ) ) {
+								rename( $source, $workdir . '.user.ini' );
+								$found = TRUE;
 DS_Clean_Import::debug(__METHOD__.'():' . __LINE__ . ' renamed "' . $source . '" to "' . $workdir . 'user.ini"');
-						}
+							}
 
-						$source = $file . DIRECTORY_SEPARATOR . 'source_site_htaccess';
+							$source = $dup_installer . DIRECTORY_SEPARATOR . $file . DIRECTORY_SEPARATOR . 'source_site_htaccess';
 DS_Clean_Import::debug(__METHOD__.'():' . __LINE__ . ' checking "' . $source . '"');
-						if ( file_exists( $source ) ) {
-							rename( $source, $workdir . '.htaccess' );
-							$found = TRUE;
+							if ( file_exists( $source ) ) {
+								rename( $source, $workdir . '.htaccess' );
+								$found = TRUE;
 DS_Clean_Import::debug(__METHOD__.'():' . __LINE__ . ' renamed "' . $source . '" to "' . $workdir . '.htaccess"');
+							}
+							if ( $found )
+								break;				// if something was found, copied - can exit the loop
 						}
-						if ( $found)
-							break;				// if something was found, copied - can exit the loop
+					} // foreach
+				} // FALSE !== $files
+
+				// look for database.sql file
+DS_Clean_Import::debug(__METHOD__.'(): checking for database.sql');
+				if ( file_exists( $workdir . 'database.sql' ) ) {
+DS_Clean_Import::debug(__METHOD__.'(): database.sql already exists');
+				} else {
+					$files = scandir( $dup_installer . DIRECTORY_SEPARATOR );
+DS_Clean_Import::debug(__METHOD__.'(): found ' . count($files) . ' files');
+	
+					foreach ( $files as $file ) {
+DS_Clean_Import::debug(__METHOD__.'(): checking file: ' . $file);
+						if ( 'dup-database__' == substr( $file, 0, 14 ) && '.sql' === substr( $file, -4 ) ) {
+							$sql_file = $dup_installer . DIRECTORY_SEPARATOR . $file;
+							rename( $sql_file, $workdir . 'database.sql' );
+DS_Clean_Import::debug(__METHOD__.'(): renamed ' . $sql_file);
+							break;
+						}
 					}
 				}
-			}
-		}
 
-DS_Clean_Import::debug(__METHOD__.'(): checking database.sql');
-		if ( file_exists( $workdir . 'database.sql' ) ) {
-DS_Clean_Import::debug(__METHOD__.'(): database.sql already exists');
-		} else {
-			$files = scandir( $workdir . 'dup-installer' . DIRECTORY_SEPARATOR );
-DS_Clean_Import::debug(__METHOD__.'(): found ' . count($files) . ' files');
-
-			foreach ( $files as $file ) {
-DS_Clean_Import::debug(__METHOD__.'(): checking file: ' . $file);
-				if ( 'dup-database__' == substr( $file, 0, 14 ) && '.sql' === substr( $file, -4 ) ) {
-					$sql_file = $workdir . 'dup-installer' . DIRECTORY_SEPARATOR . $file;
-					rename( $sql_file, $workdir . 'database.sql' );
-DS_Clean_Import::debug(__METHOD__.'(): renamed ' . $sql_file);
-					break;
-				}
-			}
-		}
+			} // is_dir( $dup_installer )
+		} // file_exists( 'wp-config' )
 
 DS_Clean_Import::debug(__METHOD__.'(): complete');
 	}
